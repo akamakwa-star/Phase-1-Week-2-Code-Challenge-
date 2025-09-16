7776131299bd44788e2a866e468112d9c7930c96
akamnovelakwa-ship-it <akam.akwa@student.moringaschool.com>
2025-09-16 16:43:24 +0300

    all changes made

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-fetch("http://localhost:5000/characters")
+//we are declaring animalList and animalDetails
+const animalList = document.getElementById("animal-list")
+const animalDetails = document.getElementById("animal-details")
+
+// fetching data from db.json then getting the response
+fetch("http://localhost:3000/characters")
 .then(response => response.json())
 .then( (data) => console.log(data))
+
+//we want to perform a function that loops through arrays
+function seeAnimals(animals){
+    // we will loop through each element using array method
+    animals.forEach(animal => {
+    //we want to create an element within the html document using the DOM manipulation
+    const li = document.createElement('li')
+        //we want to listen this element as we click it through
+        li.addEventListener('click', () => animaldetail(animal))
+        // we want to add the li element 
+        animalList.appendChild('li') 
+
+    });
+}
